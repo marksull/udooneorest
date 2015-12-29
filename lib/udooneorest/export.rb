5dbba8e6b3f37c5be43925242e1d0f894b6a63ec
Mark Sullivan <mark@sullivans.id.au>
2015-12-29 16:55:05 +1000

    Add unexport capability

--- lib/udooneorest/export.rb
+++ lib/udooneorest/export.rb
@@ -21,19 +21,35 @@ module UdooNeoRest
       export_pin params[:pin]
     end
 
+    post '/gpio/:gpio/unexport' do
+      export_gpio params[:gpio], :unexport
+    end
+
+    post '/pin/:pin/unexport' do
+      export_pin params[:pin], :unexport
+    end
+
+    get '/lazyrest/gpio/:gpio/unexport' do
+      export_gpio params[:gpio], :unexport
+    end
+
+    get '/lazyrest/pin/:pin/unexport' do
+      export_pin params[:pin], :unexport
+    end
+
     #############################################################################
     # Export using the pin
     #
     # pin     : the pin to be exported
     #
-    def export_pin(pin)
+    def export_pin(pin, mode = :export)
 
       # Validate the pin
       pin_ = ValidatePin.new pin
       return pin_.error_message unless pin_.valid?
 
       # Export the value after translating to gpio
-      export_gpio pin_.to_gpio
+      export_gpio pin_.to_gpio, mode
     end
 
     #############################################################################
@@ -41,14 +57,14 @@ module UdooNeoRest
     #
     # gpio     : the gpio to be exported
     #
-    def export_gpio(gpio)
+    def export_gpio(gpio, mode = :export)
 
       # lets make sure the gpio is good
       gpio_          = ValidateGpio.new gpio
       return gpio_.error_message unless gpio_.valid?
 
       # send the command
-      result         = UdooNeoRest::Base.echo gpio, "#{BASE_PATH}export"
+      result         = UdooNeoRest::Base.echo gpio, "#{BASE_PATH}#{'un' if mode == :unexport}export"
 
       # return ok if all is well
       return UdooNeoRest::Base.status_ok if result.empty?
